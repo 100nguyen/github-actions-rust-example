41dc2887e9d89a4a2134b65ec6a6936c2f220e90
Bach Nguyen <94183765+100nguyen@users.noreply.github.com>
2023-07-21 23:05:58 +0000

    remove greeting

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use trust::mul;
 use trust::sub;
 
 fn main() {
-    println!("Hello, world!");
     println!("add(1, 2) = {}", add(1, 2));
     println!("sub(1, 2) = {}", sub(1, 2));
     println!("mul(1, 2) = {}", mul(1, 2));
